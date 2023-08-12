db8241710b3f0e2039f590b994403113bb2192e9
DAVI BRITO SILVA E SOUZA <davi.brito@a.ucb.br>
2023-08-12 11:08:41 -0300

    add at

---
+++ atividade1/verifica_numero_primo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int verificanumeroprimo(int n){
+    return 0;
+}
+
+int main( int argc, char* argv[]){
+
+    int n;
+    printf("teste");
+
+    printf("\n%s\n", argv[1]);
+
+    
+}
